eebedd921b5574da7a6feb08f5924eb9332be628
Ghazi <ghaziatie@gmail.com>
2021-09-18 01:41:34 +0200

    rename urls app products

--- products/urls.py
+++ products/urls.py
@@ -3,11 +3,8 @@ from django.urls.conf import path
 
 from . import views # import views so we can use them in urls.
 
-
 urlpatterns = [
-    path('publisher/', views.PublisherListView.get_name),
-    path('publisher/data', views.PublisherListView.get_products),
-    path('publis/', views.PublisherListView.as_view()),
-    path('<publisher>/', views.PublisherListView.as_view()),
-  # "/store" will call the method "index" in "views.py"
+    path('dataproducts', views.PublisherListView.get_products_by_cat, name='products_cat'),
+    path('publisher/data', views.PublisherListView.get_favorite_products, name='fav_products'),
+    path('detailproduct/<int:pk>', views.PublisherListView.detail_prod, name='product_detail'),
 ]
