d665fce92fe8074d06b25fba3cf58f6205a1ea70
Ghazi <ghaziatie@gmail.com>
2021-09-18 01:53:02 +0200

    add field email

--- users/forms.py
+++ users/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class RegisterUsers(forms.Form):
     username = forms.CharField(label='username', max_length=150)
     password = forms.CharField(label='password', max_length=128, widget=forms.PasswordInput)
+    email = forms.EmailField(label='email', max_length=256)
+
