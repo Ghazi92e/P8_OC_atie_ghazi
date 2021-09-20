3e4b120f527b06161199f671fd59f329f32a4366
Ghazi <ghaziatie@gmail.com>
2021-09-20 13:13:37 +0200

    add fields fat salt sugars

--- products/models.py
+++ products/models.py
@@ -14,23 +14,25 @@ class Categories(models.Model):
         cat = cls(name=name)
         cat.save()
         return name
-
-
 class Product(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     nutriscore = models.CharField(max_length=1)
     image_product = models.CharField(max_length=200)
-    nutrition_score_100g = models.CharField(max_length=10)
+    fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    salt_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    saturated_fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    sugars_100g = models.DecimalField(max_digits=5, decimal_places=2)
+
+
     categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
     @classmethod
-    def add_products(cls, name, image_product, nutrition_score_100g, nutriscore, url, categories):
-        data = cls(name=name, image_product=image_product, nutrition_score_100g=nutrition_score_100g, nutriscore=nutriscore, url=url, categories=categories)
+    def add_products(cls, fat_100g, name, image_product, nutriscore, salt_100g, saturated_fat_100g, sugars_100g, url, categories):
+        data = cls(fat_100g=fat_100g, name=name, image_product=image_product, nutriscore=nutriscore, salt_100g=salt_100g, saturated_fat_100g=saturated_fat_100g, sugars_100g=sugars_100g, url=url, categories=categories)
         data.save()
-        return data
 class Product_favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -39,5 +41,4 @@ class Product_favorite(models.Model):
     def add_favorite_products(cls, user, product):
         favproducts = cls(user=user, product=product)
         favproducts.save()
-        print("favproducts saved !")
 
