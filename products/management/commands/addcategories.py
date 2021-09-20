1ee6309d517b7c7996a5cf41a85a5d7e42bd7acb
Ghazi <ghaziatie@gmail.com>
2021-09-20 13:17:24 +0200

    create file to add categories products

---
+++ products/management/commands/addcategories.py
@@ -0,0 +1,17 @@
+from products.models import Categories
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        categories = [
+            'pizza',
+            'drink',
+            'cheese',
+            'chocolate',
+        ]
+        for cat in categories:
+            Categories.add_categories(cat)
+
+        print("Les catégories ont été ajoutées à votre base de données !")
