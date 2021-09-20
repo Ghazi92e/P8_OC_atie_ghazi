6090107ee74e3d0951eb4fb0e82040135fa00e54
Ghazi <ghaziatie@gmail.com>
2021-09-20 13:18:31 +0200

    add fields to API

--- products/management/commands/addproducts.py
+++ products/management/commands/addproducts.py
@@ -1,5 +1,5 @@
-from products.models import Categories, Product
-from django.core.management.base import BaseCommand, CommandError
+from products.models import Product, Categories
+from django.core.management.base import BaseCommand
 import requests
 
 
@@ -20,8 +20,8 @@ class Command(BaseCommand):
                         'json': 'true',
                         'search_terms': cat,
                         'search_tag': 'categories',
-                        'fields': 'generic_name_fr,code,'
-                                    'nutrition_grades,url,stores',
+                        'fields': 'generic_name_fr,'
+                                    'nutrition_grades,url,image_url,fat_100g,salt_100g,saturated-fat_100g,sugars_100g',
                         'tag_contains_0': 'contains',
                         'page_size': 30,
                         'page': 1,
@@ -29,15 +29,14 @@ class Command(BaseCommand):
                 )
             res = r.json()
             self.products[cat] = res["products"]
+
         Command.filterdata(self)
-        #print(self.products)
-        #Categories.add_categories(cat)
-        
         for cat in self.categories:
             r = Categories.objects.get(name=cat)
             for data in self.products[cat]:
-                if len(data) > 4:
-                    Product.add_products(data["generic_name_fr"], data["url"], data["nutrition_grades"], categories=r)
+                if len(data) > 7:
+                    Product.add_products(data["fat_100g"], data["generic_name_fr"], data["image_url"], data["nutrition_grades"], data["salt_100g"], data["saturated-fat_100g"], data["sugars_100g"], data["url"], categories=r)
+        print("Les produits ont été ajoutés à votre base de données !")
 
     def filterdata(self):
         for key in self.categories:
@@ -45,13 +44,9 @@ class Command(BaseCommand):
                 try:
                     if data['generic_name_fr'] == '':
                         data.pop("generic_name_fr")
-                    if data['code'] == '':
-                        data.pop("code")
                     if data['nutrition_grades'] == '':
                         data.pop("nutrition_grades")
                     if data['url'] == '':
                         data.pop("url")
-                    if data['stores'] == '':
-                        data.pop("stores")
                 except KeyError:
                     pass
