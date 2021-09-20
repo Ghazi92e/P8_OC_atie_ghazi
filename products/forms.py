c1c74c250ef80a4588dc8603bfd35f1e50137801
Ghazi <ghaziatie@gmail.com>
2021-09-20 13:11:40 +0200

    rename form product

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class ProductForm(forms.Form):
+    product_form = forms.CharField(label='Product form', max_length=100)
 
 class IdForm(forms.Form):
     id_product = forms.CharField(label='id product', max_length=50)
