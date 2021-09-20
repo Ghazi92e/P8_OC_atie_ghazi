7442454329ff2a134d5577028a456aa77cb737a5
Ghazi <ghaziatie@gmail.com>
2021-09-20 13:16:07 +0200

    add form ProductForm

--- products/views.py
+++ products/views.py
@@ -1,19 +1,18 @@
 from django.db import reset_queries
 from products.models import Categories, Product, Product_favorite
 from django.views.generic import ListView
-from django import forms
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-from .forms import IdForm, NameForm
+from products.forms import ProductForm
 
 class PublisherListView(ListView):
       
     @login_required(login_url='/users/login/')
     def get_favorite_products(request):
-        form = NameForm(request.GET)
+        form = ProductForm(request.GET)
         data_fav = None
         current_user = request.user
         data_fav = Product_favorite.objects.filter(user_id=current_user.id)
@@ -25,18 +24,19 @@ class PublisherListView(ListView):
         
     def get_products_by_cat(request):
         query = None
+        form = None
         if request.method == 'GET':
-            form = NameForm(request.GET)
+            form = ProductForm(request.GET)
             if form.is_valid():
-                data = form.cleaned_data['your_name']
+                data = form.cleaned_data['product_form']
                 pub = get_object_or_404(Categories, name=data)
                 query = Product.objects.filter(categories=pub).order_by('nutriscore')
                 return render(request, 'products/products_by_categories.html', {'form': form, 'q': query})
             else:
-                form = NameForm()
+                form = ProductForm()
         return render(request, 'purbeurre_project/home.html', {'form': form, 'q': query})
 
     def detail_prod(request, pk):
-        form = NameForm(request.GET)
+        form = ProductForm(request.GET)
         product = Product.objects.get(pk=pk)
         return render(request, 'products/product_detail.html', context={'product': product, 'form': form })
