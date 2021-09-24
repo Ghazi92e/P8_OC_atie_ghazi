016eaad0dd653d98439d8a80a6b42f91490fe6fc
Ghazi <ghaziatie@gmail.com>
2021-09-24 23:10:54 +0200

    add comments

--- products/forms.py
+++ products/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 
 class ProductForm(forms.Form):
+    # form for search categories products
     product_form = forms.CharField(label='Product form', max_length=100)
 
 
 class IdForm(forms.Form):
+    # form to get id products
     id_product = forms.CharField(label='id product', max_length=50)
