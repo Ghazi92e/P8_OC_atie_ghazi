2850d35830e1007dcc9818565da3f49268800883
Ghazi <ghaziatie@gmail.com>
2021-10-09 21:19:20 +0200

    config __init__

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-93fhs1k-(+&ou5z!t19e(e8hyng6=25sr1b^i4a35z)mr=xi3c'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'products_purbeurre',
-        'USER': 'ghazi',
-        'PASSWORD': 'Liban',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
         'HOST': '',
         'PORT': '5432',
     }
